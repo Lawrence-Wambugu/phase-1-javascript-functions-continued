054bfe7f953fa3f148bfa67871d129983de8d30c
Lawrence-wambugu <wambugulawrence@gmail.com>
2025-03-05 19:07:02 +0300

    Edits index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    console.log(`This Saturday, I want to ${activity}!`);
+  }
+  
+  // Example usage:
+  
+  saturdayFun(); // Outputs: This Saturday, I want to roller-skate!
+  saturdayFun('go hiking'); // Outputs: This Saturday, I want to go hiking!
+  
+  function mondayWork(task = 'go to the office') {
+    console.log(`On Monday, I will ${task}.`);
+}
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Example usage:
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("amazing"));  // Output: You are !!!amazing!!!!
+  console.log(encouragingPromptFunction());          // Output: You are *special*!
+  
+  function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(task = "go to the office") {
+    console.log(`On Monday, I need to ${task}.`);
+}
+function mondayWork(activity = "go to the office") {
+    console.log(`This Monday, I will go to the office.`);
+}
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+};
